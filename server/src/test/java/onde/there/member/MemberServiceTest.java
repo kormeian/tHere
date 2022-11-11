83467f99bd1b7ee5257cdcd1947e72e63debaf39
kimjungmin <jungmini0601@gmail.com>
2022-11-11 20:33:37 +0900

    test: 아이디 중복 확인 테스트

--- server/src/test/java/onde/there/member/MemberServiceTest.java
+++ server/src/test/java/onde/there/member/MemberServiceTest.java
@@ -1,148 +1,66 @@
 package onde.there.member;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import onde.there.domain.Member;
 import onde.there.dto.member.MemberDto;
-import onde.there.member.exception.MemberException;
-import onde.there.member.exception.type.MemberErrorCode;
-import onde.there.member.security.jwt.JwtService;
+import onde.there.image.service.AwsS3Service;
+import onde.there.member.repository.MemberRepository;
 import onde.there.member.service.MemberService;
 import onde.there.member.utils.MailService;
 import onde.there.member.utils.RedisService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
 public class MemberServiceTest {
 
-    @Autowired
-    private MemberService memberService;
+    @InjectMocks
+    MemberService memberService;
 
-    @Autowired
-    private onde.there.member.repository.MemberRepository memberRepository;
+    @Mock
+    MemberRepository memberRepository;
 
-    @Autowired
-    private RedisService<Member> redisService;
-    @Autowired
-    private RedisService<String> tokenService;
+    @Mock
+    PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    @Mock
+    MailService mailService;
 
-    @Autowired
-    private JwtService jwtService;
+    @Mock
+    RedisService<Member> memberRedisService;
 
-    class TestMailService extends MailService {
+    @Mock
+    AwsS3Service awsS3Service;
 
-        public TestMailService(JavaMailSender javaMailSender) {
-            super(javaMailSender);
-        }
-
-        @Override
-        public void sendSignupMail(String uuid, Member member) {
-            System.out.println("메일 전송 기능 잘 호출 됨");
-        }
-    }
-
-    @Transactional
     @Test
-    void 아이디중복_확인_성공_케이스_중복된아이디 () {
+    void 아이디_중복_체크_성공_사용_가능한_이메일 () {
         // given
-        Member member = new Member("test2", "test2", "test2", "test2");
-        memberRepository.save(member);
+        MemberDto.CheckEmailRequest request = generateCheckEmailRequest();
+        given(memberRepository.existsByEmail(request.getEmail())).willReturn(false);
         // when
-        MemberDto.CheckIdRequest request = new MemberDto.CheckIdRequest("test2");
-        boolean result = memberService.checkId(request);
-        // then
-        assertThat(result).isFalse();
-    }
-
-    @Transactional
-    @Test
-    void 아이디중복_확인_성공_케이스_사용가능아이디 () {
-        // given
-        Member member = new Member("test2", "test2", "test2", "test2");
-        memberRepository.save(member);
-        // when
-        MemberDto.CheckIdRequest request = new MemberDto.CheckIdRequest("newId");
-        boolean result = memberService.checkId(request);
-        // then
-        assertThat(result).isTrue();
-    }
-
-    @Transactional
-    @Test
-    void 이메일중복_확인_성공_케이스_중복된아이디 () {
-        // given
-        Member member = new Member("test2", "test2", "test2", "test2");
-        memberRepository.save(member);
-        // when
-        MemberDto.CheckEmailRequest request = new MemberDto.CheckEmailRequest("test2");
-        boolean result = memberService.checkEmail(request);
-        // then
-        assertThat(result).isFalse();
-    }
-
-    @Transactional
-    @Test
-    void 이메일중복_확인_성공_케이스_사용가능아이디 () {
-        // given
-        Member member = new Member("test2", "test2", "test2", "test2");
-        memberRepository.save(member);
-        // when
-        MemberDto.CheckEmailRequest request = new MemberDto.CheckEmailRequest("newEmail");
         boolean result = memberService.checkEmail(request);
         // then
         assertThat(result).isTrue();
     }
 
-    @Transactional
     @Test
-    void 회원가입요청_성공 () {
+    void 아이디_중복_체크_성공_사용_불가능한_이메일 () {
         // given
-        TestMailService testMailService = new TestMailService(new JavaMailSenderImpl());
-        MemberDto.SignupRequest request = new MemberDto.SignupRequest("test2","test@test.com","test2","test2", "1234");
-
+        MemberDto.CheckEmailRequest request = generateCheckEmailRequest();
+        given(memberRepository.existsByEmail(request.getEmail())).willReturn(true);
         // when
-        Member member = memberService.sendSignupMail(request);
+        boolean result = memberService.checkEmail(request);
         // then
-        assertThat(member.getId()).isEqualTo(request.getId());
-        assertThat(member.getName()).isEqualTo(request.getName());
-        assertThat(member.getEmail()).isEqualTo(request.getEmail());
-        assertThat(passwordEncoder.matches("1234", member.getPassword())).isTrue();
-        assertThat(member.getNickName()).isEqualTo(request.getNickName());
+        assertThat(result).isFalse();
     }
 
-    @Transactional
-    @Test
-    void 회원가입요청_실패_중복된_이메일() {
-        //given
-        MemberDto.SignupRequest request = new MemberDto.SignupRequest("test2","test@test.com","test2","test2", "1234");
-        memberRepository.save(new Member("test2","test@test.com","1234","test2"));
-        //when
-        MemberException memberException = org.junit.jupiter.api.Assertions.assertThrows(MemberException.class,
-                () -> memberService.sendSignupMail(request));
-        //then
-        assertThat(memberException.getMemberErrorCode()).isEqualTo(MemberErrorCode.DUPLICATED_MEMBER_EMAIL);
+    private MemberDto.CheckEmailRequest generateCheckEmailRequest() {
+        return MemberDto.CheckEmailRequest.builder().email("test").build();
     }
-
-    @Transactional
-    @Test
-    void 회원가입요청_실패_중복된_아이디() {
-        //given
-        MemberDto.SignupRequest request = new MemberDto.SignupRequest("test2","test@test.com","test2","test2", "1234");
-        memberRepository.save(new Member("test2","asf@naver.com","1234","test2"));
-        //when
-        MemberException memberException = org.junit.jupiter.api.Assertions.assertThrows(MemberException.class,
-                () -> memberService.sendSignupMail(request));
-        //then
-        assertThat(memberException.getMemberErrorCode()).isEqualTo(MemberErrorCode.DUPLICATED_MEMBER_ID);
-    }
-
 }

4fecc9352f9a46238a2d3e0d37a0238f05bc79f4
Juwon <tjwndnjs1998@gmail.com>
2022-11-15 01:21:52 +0900

    Feat : Querydsl 수정 offset Pagination -> cursor Pagination 으로 변환 count query 삭제, limit에 size + 1 추가 및 다음 페이지 데이터가 있는 확인하는 hasNext, 입력받은 id 다음 부터 조회하는 eqCursorId method  추가

--- server/src/main/java/onde/there/journey/repository/JourneyRepositoryCustom.java
+++ server/src/main/java/onde/there/journey/repository/JourneyRepositoryCustom.java
@@ -4,9 +4,10 @@ import onde.there.domain.Journey;
 import onde.there.dto.journy.JourneyDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface JourneyRepositoryCustom {
 
-	Page<Journey> searchAll(JourneyDto.FilteringRequest filteringRequest, Pageable pageable);
-	Page<Journey> journeyListByMemberId(String memberId, Pageable pageable);
+	Slice<Journey> searchAll(JourneyDto.FilteringRequest filteringRequest, Pageable pageable, Long cursorId);
+	Slice<Journey> journeyListByMemberId(String memberId, Pageable pageable, Long cursorId);
 }
